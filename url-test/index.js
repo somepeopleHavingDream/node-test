bfe29dd5a8b1710888e26d57435ce88048aa3133
yangxin <1393003255@qq.com>
2025-04-28 14:26:19 +0800

    feat: 上传一些代码

---
+++ url-test/index.js
@@ -0,0 +1,3 @@
+const locationUrl = new URL('https://h5-global.v.show/rank/tipping?from=backend_test&show_title_bar=0&tab_index=0&category=rank_live_fee');
+// const locationUrl = new URL('https://h5-global.v.show/rank/tipping?from=backend_test&show_title_bar=0&tab_index=0&category=rank_live_fee');
+console.log(locationUrl.host)
